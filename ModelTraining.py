4f3c9fccb4de098579069693dc9fa75f50a343b2
Helium-bre <hehugo1024@gmail.com>
2025-02-02 05:31:33 -0500

    Added Visual_test.py and weights
    Pre-trained model with visualization program

--- ModelTraining.py
+++ ModelTraining.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-
-TESTFILE = "nb_data/46inputs_bis.csv"
+LOADFILE = 'weights/RNN1.weights.h5'
+TESTFILE = 'new_hand_landmarks_data.csv'        # FILE FOR TRAININGGGGG
+SAVEFILE = 'weights/RNN1.weights.h5'
 
 
 
@@ -57,7 +58,7 @@ X_train, y_train = np.array([ex.data for ex in training]),np.array([ex.output fo
 
 # Define RNN Model
 model = keras.Sequential([
-    keras.Input(shape = (5,44)),
+    keras.Input(shape = (5,42)),
     layers.SimpleRNN(64, activation="relu", return_sequences=True),
     layers.SimpleRNN(32, activation="relu"),  # Second RNN layer
     layers.Dense(10, activation="softmax", name = 'outputLayer')  # Output layer for classification
@@ -65,13 +66,14 @@ model = keras.Sequential([
 
 # Compile Model
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-
+if LOADFILE != None:
+    model.load_weights(LOADFILE)
 # Print Model Summary
 model.summary()
 
-history = model.fit(X_train, y_train, epochs=100, batch_size=20)#, validation_data=(X_test, y_test))
+history = model.fit(X_train, y_train, epochs=250, batch_size=32 )#, validation_data=(X_test, y_test))
 
-
+model.save_weights(SAVEFILE)
 prediction = model.predict(np.expand_dims(X_train[0], axis=0)) # PUT PREDICTION DATA HEEEEERREEEE 
 # Plot accuracy
 plt.plot(history.history['accuracy'], label="Train Accuracy")
